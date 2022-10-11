57dbe5b5c0070f4592bafd61fcf9defd62954538
skar07 <sanskarpandey0@gmail.com>
2022-10-12 03:45:34 +0530

    chore: add form to home page

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,22 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/FormContainer.module.css'
 import { Form } from '../components/Form'
 
 
 export default function Home() {
-	return (
-		<>
-			<Head>
-				<title>F&G - Aromatic Bar</title>
-			</Head>
-			<div className={styles.main}>
-				<div className={styles.headerContainer}>
-					<h2 className={styles.header}>Aromatic bar</h2>
-				</div>
-				<section>
-					<Form/>
-				</section>
-			</div>
-		</>
-	)
+      return (
+            <>
+                  <Head>
+                        <title>F&G - Aromatic Bar</title>
+                  </Head>
+                  <div className={styles.main}>
+                        <div className={styles.headerContainer}>
+                              <h2 className={styles.header}>Aromatic bar</h2>
+                        </div>
+                        <section className={styles.formContainer}>
+                              <Form />
+                        </section>
+                  </div>
+            </>
+      )
 }
