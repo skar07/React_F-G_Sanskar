11a02a038083ca2970290d7c14d880a4486f3191
skar07 <sanskarpandey0@gmail.com>
2022-10-14 20:31:51 +0530

    chore: restructure Form folder

--- components/Form/index.jsx
+++ components/Form/index.jsx
@@ -1,22 +1,42 @@
-import React from 'react'
-import styles from '../styles/Form.module.css'
-import ContainerStyle from '../styles/FormContainer.module.css'
+import React from 'react';
+import { useRouter } from 'next/router';
+import { useForm } from "react-hook-form";
+import styles from '../../styles/Form.module.css';
+import ContainerStyle from '../../styles/FormContainer.module.css';
+import { handleStorage } from "../../util/handleStorage";
+
 
 export const Form = () => {
+      const router = useRouter();
+      const { register, handleSubmit } = useForm();
+      const onSubmit = ({ name, email, phone }) => {
+            const body = { name, email, phone }
+            if (!localStorage.getItem('data')) {      
+                  localStorage.setItem('data', [JSON.stringify(body)])
+                  router.push('/table')
+                  return;
+            }
+            let temp = localStorage.getItem('data');
+            let temp_data = temp
+            temp_data.push(body)
+            console.log(temp_data)
+            localStorage.setItem('data', JSON.stringify(temp_data))
+            router.push('/table')
+      }
       return (
-            <form action='../pages/table' method='post' className={ContainerStyle.formContainer}>
+            <form onSubmit={handleSubmit(onSubmit)} className={ContainerStyle.formContainer}>
                   <div className={styles.customerInfo}>
                         <label className={styles.label}>
                               Customer Name: <br />
-                              <input id={styles.customerText} type='text' name='name' placeholder='E.g. John Doe' required />
+                              <input {...register("name")} id={styles.customerText} type='text' placeholder='E.g. John Doe' required />
                         </label>
                         <label className={styles.label}>
                               Email: <br />
-                              <input id={styles.customerText} type='email' name='email' placeholder='example@mail.com' required />
+                              <input {...register("email")} id={styles.customerText} type='email' placeholder='example@mail.com' required />
                         </label>
                         <label className={styles.label}>
                               Phone: <br />
-                              <input id={styles.customerPhone} type='tel' name='phone' placeholder='123-45-678-90' pattern='[0-9]{3}-[0-9]{2}-[0-9]{3}-[0-9]{2}' required />
+                              <input {...register("phone")} id={styles.customerPhone} type='tel' name='phone' placeholder='123-45-678-90' pattern='[0-9]{10}' required maxLength={10} minLength={10} />
                         </label>
                   </div>
                   <div className={styles.feedback}>
@@ -105,7 +125,7 @@ export const Form = () => {
                               </div>
                         </div>
                   </div>
-                        <input type='submit' value='Submit Review' id={styles.submit} />
+                  <input type='submit' value='Submit Review' id={styles.submit} />
             </form>
       )
 }
